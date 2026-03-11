538200dd3a9715d95f15994eb1ac3ce6d542c4d3
Bienvenu <bienvenumakedika@gmail.com>
2026-03-11 15:42:29 +0100

    feat: add HTTPBearer security scheme for Swagger UI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+from fastapi.security import HTTPBearer
 from app.api import products, auth
 
-app = FastAPI()
+app = FastAPI(title='Order API', version='0.1.0')
+
+security = HTTPBearer()
 
 @app.get("/")
 def home():
     return {'message': 'Order API running'}
 
-app.include_router(products.router)
 app.include_router(auth.router)
+app.include_router(products.router)
