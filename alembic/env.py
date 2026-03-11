2ebc54fdd2ba575f0e67ec480ae20190d97bee48
Bienvenu <bienvenumakedika@gmail.com>
2026-03-11 12:04:40 +0100

    chore: configure alembic env to load dotenv and Base metadata

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()  # Load environment variables from .env file
+
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -18,7 +25,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.product import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
