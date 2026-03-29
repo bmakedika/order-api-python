283634bcf570424dfd9e4d4acedbdeab4def600a
Bienvenu <bienvenumakedika@gmail.com>
2026-03-29 23:28:19 +0200

    feat(main): register invoices router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.security import HTTPBearer
-from app.api import products, auth, orders
+from app.api import products, auth, orders, invoices
 
 app = FastAPI(title='Order API', version='0.1.0')
 
@@ -13,3 +13,4 @@ def home():
 app.include_router(auth.router)
 app.include_router(products.router)
 app.include_router(orders.router)
+app.include_router(invoices.router)
